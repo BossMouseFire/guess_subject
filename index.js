02d9c152fccb4b681d13ea420eb561f86ed62a5e
Данил <abanin-02@mail.ru>
2020-11-04 00:23:58 +0400

    Added main functional

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 dictJSON = {};
+let numQuesions = 0;
 fetch("http://127.0.0.1:5500/questions.json").then(async (response) => {
     dictJSON = await response.json();
     console.log(dictJSON)
+    for (let key in dictJSON) {
+        document.getElementById('mainText').innerHTML = key;
+    }
 });
-
 searchKey = (index) => {
     for (let key in dictJSON) {
         dictJSON = dictJSON[key][index];
+        console.log('---', dictJSON);
     }
 };
 
 funcYES = () => {
     searchKey(1);
-    console.log(dictJSON);
+    numQuesions += 1;
+    if (typeof dictJSON == 'string') {
+        document.getElementById('mainText').innerHTML = dictJSON;
+    } else {
+        for (let key in dictJSON) {
+            document.getElementById('mainText').innerHTML = key;
+        }
+    }
 };
 funcNO = () => {
     searchKey(0);
-    console.log(dictJSON);
+    numQuesions += 1;
+    if (typeof dictJSON == 'string') {
+        document.getElementById('mainText').innerHTML = dictJSON;
+    } else {
+        for (let key in dictJSON) {
+            document.getElementById('mainText').innerHTML = key;
+        }
+    }
 };
