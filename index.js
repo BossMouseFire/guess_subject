793d12d1eb85eb24f3987bc1f91c5b646c7ead0d
Данил <abanin-02@mail.ru>
2020-11-04 23:52:33 +0400

    final commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ dictJSON = {};
 let numQuesions = 0;
 fetch("http://127.0.0.1:5500/questions.json").then(async (response) => {
     dictJSON = await response.json();
-    console.log(dictJSON)
     for (let key in dictJSON) {
         document.getElementById('mainText').innerHTML = key;
     }
@@ -10,29 +9,26 @@ fetch("http://127.0.0.1:5500/questions.json").then(async (response) => {
 searchKey = (index) => {
     for (let key in dictJSON) {
         dictJSON = dictJSON[key][index];
-        console.log('---', dictJSON);
     }
 };
-
-funcYES = () => {
-    searchKey(1);
+funcState = (index) => {
+    searchKey(index);
     numQuesions += 1;
     if (typeof dictJSON == 'string') {
-        document.getElementById('mainText').innerHTML = dictJSON;
+        let buttons = document.getElementById('buttons');
+        buttons.style.display = "none";
+        let repeatDiv = document.getElementById('finish');
+        repeatDiv.style.display = "flex";
+        document.getElementById('numQuesions').innerHTML = String(numQuesions) + "/10";
+        let textAnsew = document.getElementById('mainText');
+        textAnsew.innerHTML = dictJSON;
+        textAnsew.style.fontSize = "240%";
     } else {
         for (let key in dictJSON) {
             document.getElementById('mainText').innerHTML = key;
         }
     }
-};
-funcNO = () => {
-    searchKey(0);
-    numQuesions += 1;
-    if (typeof dictJSON == 'string') {
-        document.getElementById('mainText').innerHTML = dictJSON;
-    } else {
-        for (let key in dictJSON) {
-            document.getElementById('mainText').innerHTML = key;
-        }
-    }
-};
+}
+funcRepeat = () => {
+    window.location.reload();
+}
