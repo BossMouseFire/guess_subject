61cf4f77988e5e71e67f7a8238a8ce0227f2b27f
Данил <abanin-02@mail.ru>
2020-12-03 17:46:12 +0400

    added profession

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 dictJSON = {};
 let numQuesions = 0;
-fetch("http://127.0.0.1:5500/questions.json").then(async (response) => {
+fetch("http://127.0.0.1:5500/track_questions.json").then(async (response) => {
     dictJSON = await response.json();
     for (let key in dictJSON) {
         document.getElementById('mainText').innerHTML = key;
@@ -9,23 +9,31 @@ fetch("http://127.0.0.1:5500/questions.json").then(async (response) => {
 searchKey = (index) => {
     for (let key in dictJSON) {
         dictJSON = dictJSON[key][index];
+        console.log(dictJSON);
     }
 };
 funcState = (index) => {
     searchKey(index);
     numQuesions += 1;
-    if (typeof dictJSON == 'string') {
-        let buttons = document.getElementById('buttons');
-        buttons.style.display = "none";
-        let repeatDiv = document.getElementById('finish');
-        repeatDiv.style.display = "flex";
-        document.getElementById('numQuesions').innerHTML = String(numQuesions) + "/10";
-        let textAnsew = document.getElementById('mainText');
-        textAnsew.innerHTML = dictJSON;
-        textAnsew.style.fontSize = "240%";
-    } else {
-        for (let key in dictJSON) {
-            document.getElementById('mainText').innerHTML = key;
+    for (let key in dictJSON) {
+        if (typeof dictJSON[key] == 'string') {
+            let buttons = document.getElementById('buttons');
+            buttons.style.display = "none";
+            let repeatDiv = document.getElementById('finish');
+            repeatDiv.style.display = "flex";
+            document.getElementById('numQuesions').innerHTML = String(numQuesions) + "/10";
+            let textAnsew = document.getElementById('mainText');
+            let answerDescription = document.getElementById('description');
+            textAnsew.innerHTML = key;
+            answerDescription.style.display = "block";
+            descriptionText = dictJSON[key];
+            if (descriptionText.length > 368) descriptionText = descriptionText.slice(0, 368) + '...';
+            answerDescription.innerHTML = descriptionText;
+            textAnsew.style.fontSize = "200%";
+        } else {
+            for (let key in dictJSON) {
+                document.getElementById('mainText').innerHTML = key;
+            }
         }
     }
 }
